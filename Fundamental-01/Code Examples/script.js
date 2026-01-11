e5ad1114fb971b803d66286b973f0d62f3bbb5a4
De-Coder-Pritam <pritamkumar2865@gmail.com>
2026-01-11 22:00:57 +0530

    Object Declaration

--- Fundamental-01/Code Examples/script.js
+++ Fundamental-01/Code Examples/script.js
@@ -1,39 +1,55 @@
-// alert("hello world!");
-// console.log(45+13);
-if(NaN === NaN){
-    let num=12;
-}
-myfunction();
- function myfunction()   
-{
-    console.log("Hellow");
-}
-const myfunc = function (){
-    console.log("Hello expression based");
-}
-const myfun = ()=>{
-    console.log("Arrow Function");
-}
-myfunc();
-myfun();
+// // alert("hello world!");
+// // console.log(45+13);
+// if(NaN === NaN){
+//     let num=12;
+// }
+// myfunction();
+//  function myfunction()
+// {
+//     console.log("Hellow");
+// }
+// const myfunc = function (){
+//     console.log("Hello expression based");
+// }
+// const myfun = ()=>{
+//     console.log("Arrow Function");
+// }
+// myfunc();
+// myfun();
 
-let arr = ['Pritam','Sohan','Chotu'];
-console.log(arr);
-arr.push(34);
-console.log(arr);
-arr.unshift(342);
-console.log(arr);
-console.log(arr.length);
-arr.pop();
-console.log(arr);
-arr.shift();
-console.log(arr);
-console.log(45+34);
-let country="India";
-let personName="Pritam";
-let continent='Europe';
-let population=15;
-console.log(country);
-console.log(personName);
-console.log(continent);
-console.log(population);
+// let arr = ['Pritam','Sohan','Chotu'];
+// console.log(arr);
+// arr.push(34);
+// console.log(arr);
+// arr.unshift(342);
+// console.log(arr);
+// console.log(arr.length);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// console.log(45+34);
+// let country="India";
+// let personName="Pritam";
+// let continent='Europe';
+// let population=15;
+// console.log(country);
+// console.log(personName);
+// console.log(continent);
+// console.log(population);
+
+const pritam = {
+  firstName: 'Pritam',
+  lastName: 'Branwal',
+  dateOfBirth: 2002,
+  calcAge: function () {
+    this.age = 2026 - this.dateOfBirth;
+    return this.age;
+  },
+};
+console.log(pritam.calcAge());
+console.log(pritam.age);
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`Rep ${i}`);
+}
